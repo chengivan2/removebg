55a3c8dc4953329facaf290142a0dab625e88fa3
chengivan2 <chengoleivan@gmail.com>
2023-12-27 22:46:06 +0300

    Previous repo deleted. Reason: Had pushed with my API key

--- removebg.py
+++ removebg.py
@@ -1,14 +1,13 @@
-# Requires "requests" to be installed (see python-requests.org)
 import requests
 
 response = requests.post(
     'https://api.remove.bg/v1.0/removebg',
-    files={'image_file': open('c:\Users\IVAN\Downloads\493shots_so.png', 'rb')},
+    files={'image_file': open('IMAGE-PATH', 'rb')},
     data={'size': 'auto'},
-    headers={'X-Api-Key': 'BBb69YqYKEgX7w2BXRY2Bgc9'},
+    headers={'X-Api-Key': 'ENTER-YOUR-API-KEY'},
 )
 if response.status_code == requests.codes.ok:
-    with open('no-bg.png', 'wb') as out:
-        out.write(response.content)
+    with open('no-bg.png', 'wb') as out: #Replace no-bg.png with the name you want to call your resultant image
+        out.write(response.content) #Image will be saved,by default, in the same directory your code file is in
 else:
-    print("Error:", response.status_code, response.text)
+    print("Error:", response.status_code, response.text) #Throw some error message when you fail to get an OK response
